e008c98c6a772423d73b0b0e7bf50800767cae98
Matt Autrey <mautrey17@gmail.com>
2021-07-02 16:42:59 -0400

    added teacher subclass

--- public/logic/logic.js
+++ public/logic/logic.js
@@ -1,14 +1,47 @@
-import 'axios';
+// import 'axios';
 
 
 //html components
 const role = document.querySelector('.role');
 const idNum = document.querySelector('.idNum');
+const showRoster = document.querySelector('.show-roster');
+const addPerson = document.querySelector('#add-person');
+const personName = document.querySelector('.name');
+const numStudents = document.querySelector('.num-students');
 
 class SchoolMember{
-    constructor(role, name, idnum){
+    constructor(role, name){
         this.role = role;
         this.name = name;
-        this.idnum = idnum;
+        // this.idnum = idnum;
     }
 }
+
+class Teacher extends SchoolMember{
+    constructor(role, name, students){
+        super(role, name);
+        this.students = students
+    }
+
+    showGPA(){
+        alert(`${this.name} teaches ${this.students} students!`)
+    }    
+}
+
+addPerson.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('here');
+
+    let newRole = role.value;
+    let newName = personName.value;
+    let newNum = numStudents.value;
+    
+
+    if(role.value === 'teacher'){
+        teacher = new Teacher(newRole, newName, newNum)
+        const personDiv = document.createElement('p');
+        personDiv.textContent = personName.value;
+        showRoster.appendChild(personDiv);
+        teacher.showGPA();
+    }
+})
